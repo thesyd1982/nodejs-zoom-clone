117446c095c54c9fa71bab30f040860dfd71c26f
SamiDouakha <sami.douakha@gmail.com>
2021-01-19 11:33:57 +0100

    flux video delivrer par fichier el lieu et place de la webcam

--- public/script.js
+++ public/script.js
@@ -1,22 +1,22 @@
 const socket = io("/")
 const roomId = ROOM_ID
-
+const peer = new Peer(undefined, { host: "/", port: "3001" })
 const videoGrid = document.querySelector("#video-grid")
 console.log("videoGrid", videoGrid)
 
 const myVideo = document.createElement("video")
 myVideo.muted = true
 
-let myVideoStream
-navigator.mediaDevices
-  .getUserMedia({
-    video: false,
-    audio: true,
-  })
-  .then((stream) => {
-    myVideoStream = stream
-    addVideoStream(myVideo, stream)
-  })
+// let myVideoStream
+// navigator.mediaDevices
+//   .getUserMedia({
+//     video: false,
+//     audio: true,
+//   })
+//   .then((stream) => {
+//     myVideoStream = stream
+//     addVideoStream(myVideo, stream)
+//   })
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream
@@ -25,11 +25,23 @@ const addVideoStream = (video, stream) => {
   })
   videoGrid.append(video)
 }
+
+const addVideoSource = (video, source) => {
+  video.src = source
+  video.addEventListener("loadedmetadata", () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+
 const connectToNewUser = () => {
   console.log("new user")
 }
-socket.emit("join-room", roomId)
+peer.on("open", (userId) => {
+  socket.emit("join-room", roomId, userId)
+})
 
-socket.on("user-connected", () => {
-  console.log("new user")
+socket.on("user-connected", (userId, source) => {
+  console.log("The User : " + userId + " is cionnected to the room \n" + roomId)
+  addVideoSource(myVideo, source)
 })
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const { createServer } = require("http")
 const { v4: uuidv4 } = require("uuid")
-
+const fs = require("fs")
 require("dotenv").config()
 
 const app = express()
@@ -20,13 +20,48 @@ app.get("/:room", (req, res) => {
   res.status(200).render("room", { roomId: req.params.room })
 })
 
-server.listen(process.env.PORT || 1337)
+server.listen(process.env.PORT || 5000)
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log(`Server :userId_${userId} is connected to the ${roomId}`)
     socket.join(roomId)
-
-    socket.broadcast.emit("user-connected", "world")
+    let source = "/video/bbb.mp4"
+    socket.to(roomId).broadcast.emit("user-connected", userId, source)
     // io.to(roomId).broadcast.emit("user-connected")
   })
+})
+
+app.get("/video/:name", (req, res) => {
+  const name = req.params.name
+
+  const contentType = path.extname(name)
+
+  const videoPath = `./video/${name}`
+  const videoSize = fs.statSync(videoPath).size
+  let range = req.headers.range
+  if (range) {
+    const parts = range.replace("bytes=", "").split("-")
+
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] ? parseInt(parts[1], 10) : videoSize - 1
+
+    const chunkSize = end - start + 1
+    const readStrem = createReadStream(videoPath, { start, end })
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunkSize,
+      "Content-type": contentType,
+    }
+    res.writeHead(206, head)
+    readStrem.pipe(res)
+  } else {
+    const head = {
+      "Content-type": contentType,
+      "Content-Length": videoSize,
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(videoPath).pipe(res)
+  }
 })
